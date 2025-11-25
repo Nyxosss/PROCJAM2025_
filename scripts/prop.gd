5d7ea322d881b633dc57517b277b2262c856a113
Nyxosss <goncalo.s.s.alves@tecnico.ulisboa.pt>
2025-11-25 21:45:49 +0000

    Basic AI Functionality
    Added basic AI functionality
    (can test with the Scene 'player_npc_test_env')

---
+++ scripts/prop.gd
@@ -0,0 +1,12 @@
+class_name Prop
+extends StaticBody3D
+
+@onready var back_area: Area3D = $BackArea
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
