0b5a5713cd79dc6833d0d6e8d90c5e6c7ca186bb
ist190753 <martim.david@tecnico.ulisboa.pt>
2025-11-30 12:28:17 +0000

    Update pause menus

---
+++ scripts/pause.gd
@@ -0,0 +1,23 @@
+extends Node
+
+var _pause_menu: PauseMenu = preload("uid://c5y4c5v1mu8qm").instantiate()
+
+
+func _init() -> void:
+	process_mode = Node.PROCESS_MODE_ALWAYS
+	_pause_menu.return_button_pressed.connect(_on_pause_menu_return_button_pressed)
+
+
+func _unhandled_key_input(event: InputEvent) -> void:
+	if event.is_action_pressed(&"pause"):
+		if is_ancestor_of(_pause_menu):
+			remove_child(_pause_menu)
+			get_tree().paused = false
+		else:
+			add_child(_pause_menu)
+			get_tree().paused = true
+
+
+func _on_pause_menu_return_button_pressed() -> void:
+	remove_child(_pause_menu)
+	get_tree().paused = false
