214cc6ff195d8ce9afbf7204ec8f7c5aa7789f07
ist190753 <martim.david@tecnico.ulisboa.pt>
2025-11-30 20:26:18 +0000

    Added main menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,26 +1,21 @@
-class_name PauseMenu
 extends Control
 
-signal return_button_pressed
-
 var _options_menu: OptionsMenu = preload("res://scenes/options_menu.tscn").instantiate()
-
 
 func _init() -> void:
 	_options_menu.back_button_pressed.connect(_on_options_menu_back_button_pressed)
 
+func _on_start_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
-func _on_return_button_pressed() -> void:
-	return_button_pressed.emit()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().root.propagate_notification(NOTIFICATION_WM_CLOSE_REQUEST)
+	get_tree().quit()
 
 
 func _on_options_button_pressed() -> void:
 	add_child(_options_menu)
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
 
 
 func _on_options_menu_back_button_pressed() -> void:
